2166b345411282a0bc8e75af6cb793a5c8ecb2a4
Leo Rubiano <leo.a.rubiano@gmail.com>
2019-02-23 23:19:40 -0600

    finish most of the functionality

--- js/script.js
+++ js/script.js
@@ -1,54 +1,64 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
+// Create quote array
+const quotes = [
+  {
+    quote: "Programs must be written for people to read, and only incidentally for machines to execute",
+    source: "Harold Abelson",
+  },
+  {
+    quote: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live",
+    source: "John Woods",
+  },
+  {
+    quote: "I'm not a great programmer; I'm just a good programmer with great habits",
+    source: "Kent Beck",
+  },
+  {
+    quote: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    source: "Martin Fowler",
+    citation: "Refactoring: Improving the Design of Existing Code",
+    year: "1999"
+  },
+  {
+    quote: "A long descriptive name is better than a short enigmatic name. A long descriptive name is better than a long descriptive comment",
+    source: "Robert Martin",
+    citation: "Clean Code: A Handbook of Agile Software Craftsmanship",
+    year: "2008"
+  },
+  {
+    quote: "On two occasions, I have been asked [by members of Parliament], 'Pray, Mr. Babbage, if you put into the machine wrong figures, will the right answers come out?' I am not able to rightly apprehend the kind of confusion of ideas that could provoke such a question.",
+    source: "Charles Babbage",
+  },
+  {
+    quote: "Perl – The only language that looks the same before and after RSA encryption.",
+    source: "Keith Bostic",
+  }
+];
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+const generateRandomNumber = () => {
+  return Math.floor(Math.random() * quotes.length);
+}
 
+const printQuote = () => {
+  const quote = getRandomQuote();
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
+  console.log(quote);
 
+  const quoteHTML = `
+  <p class="quote"> ${quote.quote} </p>
+  <p class="source"> ${quote.source}
+    <span class="citation"> ${quote.citation} </span>
+    <span class="year"> ${quote.year} </span>
+  </p>
+  
+  `
+  
+}
 
+const getRandomQuote = () => {
+  const index = generateRandomNumber();
+  return quotes[index];
+}
 
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
